d433a84d5bd2e105375c96705cce2a166b1c7f72
Сириус <@->
2021-10-16 19:35:09 +0300

    try catch addedДобавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Задание 10:
+ Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд.
+Реализовать класс, в котором указанные значения представлены в виде свойств.
+Для свойств предусмотреть проверку корректности данных. Класс должен содержать конструктор для установки начальных значений,
+а также метод ToRadians для перевода угла в радианы.
+Создать объект на основе разработанного класса. Осуществить использование объекта в программе.
+*/
+namespace Zadanie_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввод данных
+            Console.WriteLine("Перевод градусов в радианы.");
+            Console.Write("Введите градусы: ");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите минуты:  ");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите секунды: ");
+            int sec = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Ugol ugol = new Ugol(gradus, min, sec);
+                Console.WriteLine("Угол: {0} градусов {1} минут {2} секунд = {3:f4} радиан.", ugol.Gradus, ugol.Min, ugol.Sec, ugol.ToRadians());
+                Console.ReadKey();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Введены некорректные данные. Преобразование не выполнено.");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    class Ugol
+    {
+        double gradus;
+        double min;
+        double sec;
+        public bool err;
+
+        public Ugol(int gradus=0, int min=0, int sec=0)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+
+        public double Gradus
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    gradus = value;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректные данные!");
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            get
+            {
+                return gradus;
+            }
+        }
+        public double Min
+        {
+            set
+            {
+                if ((value >= 0)&&(value<60))
+                {
+                    min = value;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректные данные!");
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            get
+            {
+                return min;
+            }
+        }
+        public double Sec
+        {
+            set
+            {
+                if ((value >= 0) && (value < 60))
+                {
+                    sec = value;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректные данные!");
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            get
+            {
+                return sec;
+            }
+        }
+
+        public double ToRadians ()
+        {
+            double radians = ((Gradus + (Min + Sec / 60) / 60) * Math.PI) / 180;
+            return radians;
+        }
+    }
+}
